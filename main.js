07fd5ef00deac689f8dfaf7b3590766fa6196e75
guipradocardoso <guilhermeprado-09@hotmail.com>
2023-09-14 21:08:40 -0300

    presenting IMC result in popup

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const form = document.querySelector("form")
 const inputWeight = document.querySelector("#weight")
 const inputHeight = document.querySelector("#height")
 
+const modalWrapper = document.querySelector(".modal-wrapper")
+const modalMessage = document.querySelector(".modal .title span")
+const modalBtnClose = document.querySelector(".modal button.close")
+
 // 3 maneira de criar e atribuir função a um evento
 
 // form.onsubmit = () => {}
@@ -16,5 +20,16 @@ form.onsubmit = function (event) {
   const weight = inputWeight.value
   const height = inputHeight.value
 
-  console.log(weight, height)
+  const result = IMC(weight, height)
+  const message = `Seu IMC é de ${result}`
+
+  modalMessage.innerText = message
+  modalWrapper.classList.add("open")
+}
+
+// arrow function
+modalBtnClose.onclick = () => modalWrapper.classList.remove("open")
+
+function IMC(weight, height) {
+  return (weight / (height / 100) ** 2).toFixed(2)
 }
