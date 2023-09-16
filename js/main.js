c3f1ccca9c4fc0f98f6199996922d3308234f733
guipradocardoso <guilhermeprado-09@hotmail.com>
2023-09-16 02:02:06 -0300

    refactoring clean code rules

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import { Modal } from "./modal.js"
 import { AlertError } from "./alert-error.js"
-import { IMC, notANumber } from "./utils.js"
+import { calculateIMC, notANumber } from "./utils.js"
 
 // variables
 
@@ -19,16 +19,20 @@ form.onsubmit = function (event) {
 
   const weight = inputWeight.value
   const height = inputHeight.value
-  const showAlertError = notANumber(weight) || notANumber(height)
+  const weightOrHeightIsNotANumber = notANumber(weight) || notANumber(height)
 
-  if (showAlertError) {
+  if (weightOrHeightIsNotANumber) {
     AlertError.open()
     return
   }
 
   AlertError.close()
 
-  const result = IMC(weight, height)
+  const result = calculateIMC(weight, height)
+  displayResultMessage(result)
+}
+
+function displayResultMessage(result) {
   const message = `Seu IMC é de ${result}`
 
   Modal.message.innerText = message
