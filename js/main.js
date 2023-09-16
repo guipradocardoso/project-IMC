008f2dd43ea067ecf0210a78365197071fab6d18
guipradocardoso <guilhermeprado-09@hotmail.com>
2023-09-16 01:52:04 -0300

    modularize utility functions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { Modal } from "./modal.js"
 import { AlertError } from "./alert-error.js"
+import { IMC, notANumber } from "./utils.js"
 
 // variables
 
@@ -32,12 +33,4 @@ form.onsubmit = function (event) {
 
   Modal.message.innerText = message
   Modal.open()
-}
-
-function notANumber(value) {
-  return isNaN(value) || value == ""
-}
-
-function IMC(weight, height) {
-  return (weight / (height / 100) ** 2).toFixed(2)
 }
